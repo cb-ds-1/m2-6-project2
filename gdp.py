875aca3c19c7a9b5d56ac265646ffc3ae1822e80
murphman300 <jlmy@DESKTOP-T5C6DOJ.localdomain>
2020-12-21 20:44:09 -0500

    Fixed gdp file

--- gdp.py
+++ gdp.py
@@ -1,97 +1,57 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 import sys
-
-
 """
 USAGE:
 
 1) Download the following table
-    - https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3610011201 : be sure to use the "Download Entire table" option"
-2) make sure you save it as 'canadian-gdp-1960-2020.csv' file inside of the data folder
+    - https://www150.statcan.gc.ca/t1/tbl1/en/cv.action?pid=3610043401 : be sure to use the "Download Entire table" option"
+2) make sure you save it as 'gdp-canada.csv' file inside of the data folder
 3) create actionable dataframe like so:
 
 import gdp as getgdp
 
 gdp = getgdp()
 
+NB: all values are in millions
+
+getgdp takes in 1 optional argument, if you specify one, it must be a potential NAICS industry name (not including it's classification code)
+if no option is provided, a dict is returned with all Industries read in the file as keys, whose values are DataFrame objects of their 
+Period and Values.
+
+Key: Industry
+Value:
+ ________________
+| Period | Value |
+__________________
+|  Date  | Float |
 """
 
-pivot = 'Current and capital accounts - Households, Canada, quarterly'
+def unique_cols(gdp):
+    uniques = gdp['Class'].unique()
+    return uniques
 
-gdp = pd.read_csv('data/canadian-gdp-1960-2020.csv')
+def cleanup_gdp(gdp):
+    gdp = gdp.rename(columns={'North American Industry Classification System (NAICS)': 'Class',
+                             'REF_DATE': 'Period',
+                             'VALUE': 'Value',
+                             'Seasonal adjustment': 'Adj'})
+    gdp['Period'] = pd.to_datetime(gdp['Period'])
+    gdp = gdp[gdp.Adj == 'Seasonally adjusted at annual rates']
+    gdp = gdp[gdp.Prices == '2012 constant prices']
+    gdp = gdp.drop(columns=['Adj', 'Prices'])
+    gdp['Class'] = gdp['Class'].map(lambda x: " ".join(x.split(" ")[:-1]))
+    def cleanup(gdp):
+        gdp = gdp.drop(columns=['Class'])
+        return gdp
+    frames = {industry: cleanup(gdp[gdp['Class'] == industry]) for industry in gdp['Class'].unique()}
+    return frames
 
-
-def truncate_top_bottom():
-    global gdp
-    print(gdp)
-    """
-    We replace nan's with empty strings in the first column
-    """
-    gdp[pivot] = gdp[pivot].replace(np.nan, '', regex=True)
-    """
-    First find where to truncate the top part of the 
-    """
-    topRow = gdp[pivot].loc[lambda x: x=='Estimates'].index
-    """
-    Then we find as of where to truncate the bottom part of the frame
-    """
-    bottomRow = gdp[pivot].loc[lambda x: x=='Symbol legend:'].index
-    lastRow = gdp[gdp[pivot].str.contains("DOI:")].index
-    dollarRows = gdp[gdp['Unnamed: 1'] == 'Dollars'].index
-    percRows = gdp[gdp['Unnamed: 1'] == 'Percent'].index
-    rowsToDrop = [i for i in range(topRow[0])]
-    rowsToDrop += list(dollarRows)
-    rowsToDrop += list(percRows)
-    rowsToDrop += [i for i in range(bottomRow[0] - 1, lastRow[0] + 1)]
-    gdp.drop(rowsToDrop, inplace=True)
-
-def reassign_column_names():
-    global gdp
-    names = {}
-    for column in gdp.columns:
-        if column == pivot:
-            names[pivot] = 'Period'
-        else:
-            names[str(column)] = gdp.iloc[0][column]
-    gdp = gdp.rename(columns=names)
-    estimatesRow = gdp['Period'].loc[lambda x: x=='Estimates'].index
-    gdp.drop(estimatesRow, inplace=True)
-    preTColumns = gdp.columns
-    gdp = gdp.transpose()
-    newNames = {}
-    for column in gdp.columns:
-        """
-        This will cleanup the column names
-        """
-        newNames[column] = gdp.iloc[0][column].split(' (')[0]
-    gdp = gdp.rename(columns=newNames)
-    gdp.drop(['Period'], inplace=True)
-    
-def apply_new_indexes():
-    global df
-    global gdp
-    gdp['Quarter Year'] = gdp.index
-    def quarter_year(row):
-        splits = row['Quarter Year'].split(' ')
-        return pd.Series(data={
-            'Year': splits[1],
-            'Quarter': splits[0]
-        })
-    gdp = gdp.merge(gdp.apply(lambda row: quarter_year(row), axis=1), left_index=True, right_index=True)
-    gdp = gdp.set_index('Year')
-    
-
-def cleanup_gdp():
-    truncate_top_bottom()
-    reassign_column_names()
-    apply_new_indexes()
-        
-def get_gdp_df():
-    global gdp
-    cleanup_gdp()
-    return gdp
+def get_gdp_df(for_ind=None):
+    cols_to_use = ['North American Industry Classification System (NAICS)', 'REF_DATE', 'VALUE', 'Seasonal adjustment', 'Prices']
+    gdp = pd.read_csv('data/gdp-canada.csv',usecols =cols_to_use)
+    gdp = cleanup_gdp(gdp)
+    return gdp if for_ind == None else gdp[for_ind]
 
 
 sys.modules[__name__] = get_gdp_df
